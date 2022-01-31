61694deac2c14b39df0cce6ed45078ff4d61317a
Orazio <orazioventrella@hotmail.it>
2022-01-31 17:34:35 +0100

    feat: add route and function delete

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -62,6 +62,7 @@ class HomeController extends Controller
     public function delete($id) {
 
         $comic = comic::findOrFail($id);
-        return view('pages.home');
+        $comic -> delete();
+        return redirect() -> route('home');
     }
 }
